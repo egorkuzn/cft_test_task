7fe0da67031c67c41c22a55d25c645c13fcf5e62
egorkuzn <egorkuzn2002@mail.ru>
2022-09-20 14:14:46 +0700

    Initial commit

---
+++ src/main/java/com/example/cft_test_task/CftTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cft_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CftTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CftTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cft_test_task/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.example.cft_test_task.config;
+
+public class SwaggerConfig {
+}
